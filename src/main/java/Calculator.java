06a0cb34f59ca0c33930dcab7474f8cbc10f31ec
kushal <kushalagrawal40@gmail.com>
2022-04-13 16:15:10 +0530

    Factorial functionality added

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,2 +1,48 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Calculator {
+
+    public Calculator() {
+    }
+    public static  void main(String[] args)
+    {
+        Calculator calculator= new Calculator();
+        Scanner scanner=new Scanner(System.in);
+        double n1,n2;
+        System.out.println("Calculator has started");
+
+        do{
+
+            System.out.println("Enter your choice");
+            System.out.println("1.Factorial\n");
+            int opt;
+            try{
+                opt= scanner.nextInt();
+            }catch (InputMismatchException error)
+            {
+                return ;
+            }
+        switch (opt)
+        {
+            case 1://1 for factorial
+            System.out.println("Enter Number");
+            n1=scanner.nextDouble();
+            System.out.println("Factorial of"+n1+"is:"+calculator.factorial(n1)+"\n");
+            break;
+
+
+            default:System.out.println("exiting calculator");
+            return;
+        }
+        }while(true);
+    }
+
+    public double factorial(double ny) {
+        double result=1 ;
+        for(int i = 1; i <= ny; ++i)
+        { result *= i;   }
+        return result;
+    }
+
 }
